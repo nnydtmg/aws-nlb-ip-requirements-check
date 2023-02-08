df987c3dec50808dbbdd866aba6e1081a462fca2
nnydtmg <nnydtmg@gmail.com>
2023-02-08 23:01:33 +0900

    This commitment Creates VPC Resouces by L2 Construct.

--- lib/nlb-ip-requirements-check-stack.ts
+++ lib/nlb-ip-requirements-check-stack.ts
@@ -1,16 +1,26 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
 
-export class NlbIpRequirementsCheckStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class NlbIpRequirementsCheckStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'NlbIpRequirementsCheckQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      cidr: '10.1.0.0/16',
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          cidrMask: 28,
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 28,
+          name: 'private',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    })
   }
 }
