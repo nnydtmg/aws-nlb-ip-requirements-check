d2d16c4100aa21bc50256781dc8af9da4907be6f
nnydtmg <nnydtmg@gmail.com>
2023-02-09 22:49:26 +0900

    modify bin/nlb-ip-requirements-check.ts synthesizer part

--- bin/nlb-ip-requirements-check.ts
+++ bin/nlb-ip-requirements-check.ts
@@ -1,21 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { NlbIpRequirementsCheckStack } from '../lib/nlb-ip-requirements-check-stack';
+import { DefaultStackSynthesizer } from 'aws-cdk-lib';
 
 const app = new cdk.App();
 new NlbIpRequirementsCheckStack(app, 'NlbIpRequirementsCheckStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  synthesizer: new DefaultStackSynthesizer({
+    fileAssetsBucketName: 'cdk-nlb-ip-check-assets-bucket', // bootstrapのバケット名を指定
+  }),
+  description: 'This is a cdk-nlb-ip-check-stack.', // CloudFormationスタックに説明を追加
 });
--- lib/nlb-ip-requirements-check-stack.ts
+++ lib/nlb-ip-requirements-check-stack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
+import { CfnOutput } from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
 
 export class NlbIpRequirementsCheckStack extends Stack {
@@ -33,5 +34,16 @@ export class NlbIpRequirementsCheckStack extends Stack {
     const cfnPrivateSubnet2 = vpc.privateSubnets[1].node.defaultChild as ec2.CfnSubnet
     cfnPrivateSubnet2.addPropertyOverride('CidrBlock', `10.1.3.0/28`)
     */
+    
+     // CloudFormationに出力
+     // new CfnOutput(this, 'S3', { value: bucket.bucketName });
+     new CfnOutput(this, 'VPC', { value: vpc.vpcId });
+     new CfnOutput(this, 'PublicSubnet1', { value: vpc.publicSubnets[0].subnetId });
+     new CfnOutput(this, 'PublicSubnet2', { value: vpc.publicSubnets[1].subnetId });
+     new CfnOutput(this, 'PrivateSubnet1', { value: vpc.privateSubnets[0].subnetId });
+     new CfnOutput(this, 'PrivateSubnet2', { value: vpc.privateSubnets[1].subnetId });
+     // new CfnOutput(this, 'Security Group', { value: securityGroup.securityGroupId });
+     // new CfnOutput(this, 'EC2Instance1', { value: instance1.instanceId });
+     // new CfnOutput(this, 'EC2Instance2', { value: instance2.instanceId });
   }
 }
