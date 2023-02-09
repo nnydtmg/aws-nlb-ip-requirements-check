4bdff12c322a73d16f7536260ed347d3ebd02f6e
nnydtmg <nnydtmg@gmail.com>
2023-02-09 22:26:35 +0900

    modify asset bucket

--- bin/nlb-ip-requirements-check.ts
+++ bin/nlb-ip-requirements-check.ts
@@ -2,16 +2,9 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { NlbIpRequirementsCheckStack } from '../lib/nlb-ip-requirements-check-stack';
-import { DefaultStackSynthesizer } from "@aws-cdk/core";
 
 const app = new cdk.App();
-const fileAssetsBucketName = app.node.tryGetContext("cdk-bootstrap-nnydtmg-bucket");
 new NlbIpRequirementsCheckStack(app, 'NlbIpRequirementsCheckStack', {
-  /*
-  synthesizer: new DefaultStackSynthesizer({
-    fileAssetsBucketName: fileAssetsBucketName
-  }),
-  */
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
