f430671584002f3b65c5127f07b0eaa75f557534
nnydtmg <nnydtmg@gmail.com>
2023-02-10 00:09:42 +0900

    Update Source Bucket

--- lib/nlb-ip-requirements-check-stack.ts
+++ lib/nlb-ip-requirements-check-stack.ts
@@ -1,15 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { CfnOutput } from 'aws-cdk-lib';
-import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class NlbIpRequirementsCheckStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    const vpc = new ec2.Vpc(this, 'VPC', {
+    const vpc = new ec2.Vpc(this, 'nlbVPC', {
       cidr: '10.1.0.0/16',
       maxAzs: 3,
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
       subnetConfiguration: [
         {
           cidrMask: 28,
@@ -18,7 +20,12 @@ export class NlbIpRequirementsCheckStack extends Stack {
         },
         {
           cidrMask: 28,
-          name: 'private',
+          name: 'private-web',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+        {
+          cidrMask: 28,
+          name: 'private-db',
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
         },
       ],
@@ -28,6 +35,8 @@ export class NlbIpRequirementsCheckStack extends Stack {
     cfnPublicSubnet1.addPropertyOverride('CidrBlock', `10.1.0.0/28`)
     const cfnPublicSubnet2 = vpc.publicSubnets[1].node.defaultChild as ec2.CfnSubnet
     cfnPublicSubnet2.addPropertyOverride('CidrBlock', `10.1.1.0/28`)
+    const cfnPublicSubnet3 = vpc.publicSubnets[2].node.defaultChild as ec2.CfnSubnet
+    cfnPublicSubnet3.addPropertyOverride('CidrBlock', `10.1.2.0/28`)
     /*
     const cfnPrivateSubnet1 = vpc.privateSubnets[0].node.defaultChild as ec2.CfnSubnet
     cfnPrivateSubnet1.addPropertyOverride('CidrBlock', `10.1.2.0/28`)
@@ -40,8 +49,9 @@ export class NlbIpRequirementsCheckStack extends Stack {
      new CfnOutput(this, 'VPC', { value: vpc.vpcId });
      new CfnOutput(this, 'PublicSubnet1', { value: vpc.publicSubnets[0].subnetId });
      new CfnOutput(this, 'PublicSubnet2', { value: vpc.publicSubnets[1].subnetId });
-     new CfnOutput(this, 'PrivateSubnet1', { value: vpc.privateSubnets[0].subnetId });
-     new CfnOutput(this, 'PrivateSubnet2', { value: vpc.privateSubnets[1].subnetId });
+     new CfnOutput(this, 'PublicSubnet3', { value: vpc.publicSubnets[2].subnetId });
+     // new CfnOutput(this, 'PrivateSubnet1', { value: vpc.privateSubnets[0].subnetId });
+     // new CfnOutput(this, 'PrivateSubnet2', { value: vpc.privateSubnets[1].subnetId });
      // new CfnOutput(this, 'Security Group', { value: securityGroup.securityGroupId });
      // new CfnOutput(this, 'EC2Instance1', { value: instance1.instanceId });
      // new CfnOutput(this, 'EC2Instance2', { value: instance2.instanceId });
