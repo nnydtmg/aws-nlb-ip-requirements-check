b4ce224281a9f866539c9ff75f87166b0ee8d274
nnydtmg <nnydtmg@gmail.com>
2023-02-12 13:51:27 +0900

    comment out fileassetsbucketname

--- bin/nlb-ip-requirements-check.ts
+++ bin/nlb-ip-requirements-check.ts
@@ -5,9 +5,11 @@ import { DefaultStackSynthesizer } from 'aws-cdk-lib';
 
 const app = new cdk.App();
 new NlbIpRequirementsCheckStack(app, 'NlbIpRequirementsCheckStack', {
+  /*
   synthesizer: new DefaultStackSynthesizer({
     fileAssetsBucketName: 'cdk-assets-nnydtmg-bucket', // bootstrapのバケット名を指定
   }),
+  */
   description: 'This is a cdk-nlb-ip-check-stack.', // CloudFormationスタックに説明を追加
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
